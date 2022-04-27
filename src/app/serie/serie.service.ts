c4744352655674e6f8c2209ce0c9b21622ad4c81
BraitoRX <69652100+BraitoRX@users.noreply.github.com>
2022-04-27 01:53:55 -0500

    full taller Angular

---
+++ src/app/serie/serie.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { serie } from './serie';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class SerieService {
+  constructor(private http: HttpClient) { }
+  getSeries(): Observable<serie[]> {
+    return this.http.get<serie[]>("https://gist.githubusercontent.com/josejbocanegra/8490b48961a69dcd2bfd8a360256d0db/raw/34ff30dbc32392a69eb0e08453a3fc975a3890f0/series.json");
+  }
+  
+}
